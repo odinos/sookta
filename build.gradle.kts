ccd5fdbf839f08973595462607de79fad3df07c1
Methee Treewichian <140489833+methee60893@users.noreply.github.com>
2026-01-01 21:16:02 +0700

    kts file

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,7 @@ import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 
 plugins {
     alias(libs.plugins.android.application)
+    //id("com.android.application") version "8.1.4" apply false
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose.compiler)
     alias(libs.plugins.ksp)
